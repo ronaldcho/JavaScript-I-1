73ac2d65f09f400abbb2676a0d3beb99c287e208
Ronald Cho <ronaldcho@gmail.com>
2019-04-16 18:53:58 -0700

    ES6 functions convert

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -5,26 +5,59 @@
 // };
 // myFunction();
 
+
+// ES6 version
+// #######################################################################################################
+// let myFunction = function () {};
+let myFunction = () => {};
+
+
+
+let myFunction = () => {
+  console.log("Function was invoked!");
+};
+myFunction();
+
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+let anotherFunction = (param) => {
+  return param;
+};
+
+// let anotherFunction = (param) => param;
+
+anotherFunction("Example");
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
+let add = (param1, param2) => {
+  param1 + param2
+};
+
+add(1, 2);
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
+let subtract = (param1, param2) => {
+  param1 - param2
+};
 
+subtract(1, 2);
 
 // Stretch
 
-// exampleArray = [1,2,3,4];
+exampleArray = [1, 2, 3, 4];
 // const triple = exampleArray.map(function (num) {
 //   return num * 3;
 // });
 // console.log(triple);
+
+const triple = exampleArray.map(num => num * 3);
+
+console.log(triple);
---
+++ assignments/tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+exampleArray = [1, 2, 3, 4];
